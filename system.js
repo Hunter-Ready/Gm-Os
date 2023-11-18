fb998af93e8ece8b484cbe0374d1a2de78ce245b
Hunter-Ready <80565172+Hunter-Ready@users.noreply.github.com>
2023-11-18 12:06:42 -0600

    Update system.js

--- system.js
+++
@@ -1,9 +0,0 @@
-const sysCfg = require('./g')
-console.log("sysCfg")
-if (sysCfg = undefined || sysCfg = null) {
-	console.clear()
-	console.log("SYSTEM CRASH. ERROR ID CSE1")
-} else {
-	console.log("System Walware and Disk scan Completed!")
-	consold.log("fixing problems...")
-	console.log("done!")
